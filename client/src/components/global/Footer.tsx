6a01f5c9d5efcbd188642831fc8d8bf8fc915881
ภูมิพันธ์ สืบภา <poohmiphan@outlook.com>
2021-11-08 12:41:42 +0700

    make it better

---
+++ client/src/components/global/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>JOB FINDDER</h6>
+      <p><a href="https://www.facebook.com/JOB-Findder-108413164959026">ติดต่อพวกเราได้ที่เพจ facebook</a></p>
+    </div>
+  )
+}
+
+export default Footer
